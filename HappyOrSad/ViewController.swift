73ae6459684f6b8bedd6000b61e64ad751ab43e8
Wellesley Yu <byu@lcs.on.ca>
2019-11-18 11:22:12 -0500

    Initial Commit

---
+++ HappyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyOrSad
+//
+//  Created by Yu, Boning on 2019-11-18.
+//  Copyright © 2019 Wellesley Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
